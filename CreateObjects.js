50839c53d26cf3f0987382acd23f8f9bc776064e
saaisankar <81905025+saaisankar@users.noreply.github.com>
2021-06-02 18:22:41 +0530

    Add files via upload

--- CreateObjects.js
+++ CreateObjects.js
@@ -7,5 +7,26 @@ function person(name, age, color) {
 var p1 = new person("John", 42, "green");
 var p2 = new person("Amy", 21, "red");
 
-document.write(p1.age);
-document.write(p2.name);
+
+// document.write(p1.age + "<br>");
+// document.write(p2.name);
+
+
+
+
+var person = {
+    name: "John",
+    age: 31,
+    favColor: "green",
+    height: 183
+};
+// var x = person.age;
+// var y = person['height'];
+
+
+for (var i = 0 in person) {
+    console.log(person[i]);
+}
+
+// document.write(x + "<br>");
+// document.write(y);
